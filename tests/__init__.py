2df12cee3dbeb4def0b7fba61c01c10eef598ba7
Ben McGinnes <ben@adversary.org>
2015-09-07 19:46:48 +1000

    Python 2 and 3 tests
    * If this works then the same trick ought to work for the main module
      and I can get rid of the Python 3 only version (will keep the py2
      version due to 2.6 vs. 2.7 conflicts).

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,8 @@
-from .gdshortener_test import GDShortenerTest
+import sys
+
+if sys.version_info[0] == 2:
+    from gdshortener_test_py2 import GDShortenerTest
+elif sys.version_info[0] == 3:
+    from .gdshortener_test_py3 import GDShortenerTest
+else:
+    print("Something broke.")
