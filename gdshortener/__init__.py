f202250e3e9d597d4eeb59760f2b777f81482cad
Ben McGinnes <ben@adversary.org>
2015-09-07 20:00:48 +1000

    Python 2.7 and above module.
    * In theory, this should work.
    * It's possible that pip won't like it regardless, though python
      setup.py install should be fine.
    * Now it just needs testing.

--- gdshortener/__init__.py
+++ gdshortener/__init__.py
@@ -1,10 +1,26 @@
-from .gdshortener import _IS_GD_SHORTENER_URL_
-from .gdshortener import _V_GD_SHORTENER_URL_
-from .gdshortener import GDBaseException
-from .gdshortener import GDMalformedURLError
-from .gdshortener import GDShortURLError
-from .gdshortener import GDRateLimitError
-from .gdshortener import GDGenericError
-from .gdshortener import GDBaseShortener
-from .gdshortener import ISGDShortener
-from .gdshortener import VGDShortener
+import sys
+
+if sys.version_info[0] == 2:
+    from gdshortener_py2 import _IS_GD_SHORTENER_URL_
+    from gdshortener_py2 import _V_GD_SHORTENER_URL_
+    from gdshortener_py2 import GDBaseException
+    from gdshortener_py2 import GDMalformedURLError
+    from gdshortener_py2 import GDShortURLError
+    from gdshortener_py2 import GDRateLimitError
+    from gdshortener_py2 import GDGenericError
+    from gdshortener_py2 import GDBaseShortener
+    from gdshortener_py2 import ISGDShortener
+    from gdshortener_py2 import VGDShortener
+elif sys.version_info[0] == 3:
+    from .gdshortener_py3 import _IS_GD_SHORTENER_URL_
+    from .gdshortener_py3 import _V_GD_SHORTENER_URL_
+    from .gdshortener_py3 import GDBaseException
+    from .gdshortener_py3 import GDMalformedURLError
+    from .gdshortener_py3 import GDShortURLError
+    from .gdshortener_py3 import GDRateLimitError
+    from .gdshortener_py3 import GDGenericError
+    from .gdshortener_py3 import GDBaseShortener
+    from .gdshortener_py3 import ISGDShortener
+    from .gdshortener_py3 import VGDShortener
+else:
+    print("Something broke.")
